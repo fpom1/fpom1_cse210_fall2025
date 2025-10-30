23589966370f481878c6667897cc43b73b855b76
fpom1 <jen21017@byui.edu>
2025-10-30 09:13:12 -0600

    prep1_alter

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("please enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
+        
     }
 }
