5d122dce009d150ef30d3d9e6100a23bdb920c9a
fpom1 <jen21017@byui.edu>
2025-11-02 13:05:59 -0700

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string answer;
+        string LetterGrade;
+
+        Console.Write("please enter your score as a percentage.");
+        answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        if (percent >= 90)
+        {
+            LetterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            LetterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            LetterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            LetterGrade = "D";
+        }
+        else
+        {
+            LetterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {LetterGrade}");
     }
 }
+
