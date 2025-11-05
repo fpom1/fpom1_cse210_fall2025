321066df6c285151195ec100d15174b476359148
fpom1 <jen21017@byui.edu>
2025-11-05 15:52:13 -0700

    number guess game while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,44 +1,29 @@
 using System;
-
+using System.Xml.Serialization;
 class Program
 {
     static void Main(string[] args)
     {
-                for (int i = 0; i < 10; i++)
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(1, 100);
+        int guess = -99999;
+        while (guess != randomNumber)
         {
-            Console.WriteLine($"the value of i is {i}");
-        }
-        for (int i = 0; i < 1001; i += 10)
-        {
-            Console.WriteLine($"the value of i is {i}");
-        }
-
-        //while
-        bool done = false;
-
-        while (!done)
-        {
-            Console.Write("input your age");
-            int age = int.Parse(Console.ReadLine());
-            if (age > 0 && age < 130)
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > randomNumber)
             {
-                done = true;
-                Console.WriteLine($"super age {age}");
+                Console.Write("Lower\n");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.Write("Higher\n");
+            }
+            else
+            {
+                Console.Write("good job");
             }
         }
-
-        done = false;
-        //do while will always run at least once
-        do
-        {
-            Console.Write("input your age");
-            int age = int.Parse(Console.ReadLine());
-            if (age > 0 && age < 130)
-            {
-                done = true;
-                Console.WriteLine($"super age {age}");
-            }
-        } while (!done);
 
     }
 }
