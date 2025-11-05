78e073925204e85535387462198ee102753b89e8
fpom1 <jen21017@byui.edu>
2025-11-04 23:02:17 -0700

    thingo

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+                for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"the value of i is {i}");
+        }
+        for (int i = 0; i < 1001; i += 10)
+        {
+            Console.WriteLine($"the value of i is {i}");
+        }
+
+        //while
+        bool done = false;
+
+        while (!done)
+        {
+            Console.Write("input your age");
+            int age = int.Parse(Console.ReadLine());
+            if (age > 0 && age < 130)
+            {
+                done = true;
+                Console.WriteLine($"super age {age}");
+            }
+        }
+
+        done = false;
+        //do while will always run at least once
+        do
+        {
+            Console.Write("input your age");
+            int age = int.Parse(Console.ReadLine());
+            if (age > 0 && age < 130)
+            {
+                done = true;
+                Console.WriteLine($"super age {age}");
+            }
+        } while (!done);
+
     }
 }
