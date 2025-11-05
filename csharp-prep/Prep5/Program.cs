0266c2311d87895b919de8f34aa9ccc46aed421b
fpom1 <jen21017@byui.edu>
2025-11-05 16:28:23 -0700

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("welcome to the program");
+    }
+    static string promptUserName()
+    {
+        Console.WriteLine("please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int promptNumber()
+    {
+        Console.WriteLine("please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int promptYear()
+    {
+        Console.WriteLine("please enter your birth year: ");
+        int year = int.Parse(Console.ReadLine());
+        return year;
+    }
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void displayResult(string name, int year, int square)
+    {
+        int age = 2025 - year;
+        Console.WriteLine($"{name}, the square of your number is {square} ");
+        Console.WriteLine($"{name}, you will turn {age} this year.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = promptUserName();
+        int number = promptNumber();
+        int birthYear = promptYear();
+        int square = squareNumber(number);
+        displayResult(userName, birthYear, square);
     }
 }
