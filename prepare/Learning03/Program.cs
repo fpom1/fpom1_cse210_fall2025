e2fd8586fb7eaea0d3c33126447e4a073e408ef9
fpom1 <jen21017@byui.edu>
2025-11-12 09:55:28 -0700

    a

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public double GetValue()
+    {
+        return (double)_numerator / (double)_denominator;
+    }
+
+}
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction(5,7);
+        Console.WriteLine(fraction.GetValue());
     }
 }
